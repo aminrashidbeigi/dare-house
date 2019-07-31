88638128f7d404e1f436d9023b1a04a1b5a7a2e2
Amin Rashidbeigi <a.rashidbeigi@gmail.com>
2019-07-31 20:30:15 +0430

    add warehouse app

--- back-end/darehouse/settings.py
+++ back-end/darehouse/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'warehouse.apps.WarehouseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'darehouse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'darehouse',
+        'USER': 'amin',
+        'PASSWORD': 'amin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
