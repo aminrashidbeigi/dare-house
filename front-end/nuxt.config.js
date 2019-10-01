d006a849d0cb9e86c0dddd86d70edb9a1aefecc8
Amin Rashidbeigi <a.rashidbeigi@gmail.com>
2019-10-01 21:15:43 +0330

    Change port and ip of front

--- front-end/nuxt.config.js
+++ front-end/nuxt.config.js
@@ -17,6 +17,10 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  server: {
+    port: 3000, // default: 3000
+    host: '0.0.0.0', // default: localhost
+  },
   /*
   ** Customize the progress-bar color
   */
